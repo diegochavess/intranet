1627544a2809bf968b077d3f49d147784f83119e
Yasmin <Yasmin@DESKTOP-E9A12S9>
2018-05-18 00:00:37 -0300

    Primeiro commit do projeto

---
+++ js/sb-admin-2.js
@@ -0,0 +1,60 @@
+$(function() {
+    $('#side-menu').metisMenu();
+});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function() {
+    $(window).bind("load resize", function() {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    // var element = $('ul.nav a').filter(function() {
+    //     return this.href == url;
+    // }).addClass('active').parent().parent().addClass('in').parent();
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).addClass('active').parent();
+
+    while (true) {
+        if (element.is('li')) {
+            element = element.parent().addClass('in').parent();
+        } else {
+            break;
+        }
+    }
+});
+jQuery(function($){
+  $('.tabs-menu ul li a').click(function(){
+    var a = $(this);
+    var active_tab_class = 'active-tab-menu';
+    var the_tab = '.' + a.attr('data-tab');
+    
+    $('.tabs-menu ul li a').removeClass(active_tab_class);
+    a.addClass(active_tab_class);
+    
+    $('.tabs-content .tabs').css({
+      'display' : 'none'
+    });
+    
+    $(the_tab).show();
+    
+    return false;
+  });
+});
